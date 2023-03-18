2ffb274d4709d0ce3ec89d3f317e1a28d49823bc
Adisa Jazvin <adisajazvin1994@gmail.com>
2023-03-18 14:58:55 +0100

    Treci cas kra

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,53 +1,59 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-product-list',
-    templateUrl: './product-list.component.html'
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
 })
-
 export class ProductListComponent {
-    products: any[] = [
-        {
-            productId: 1,
-            productName: "Kafa",
-            productCode: "K-12",
-            category: "Topli napici",
-            price: 2,
-            rating: 4.8,
-            imageUrl: "assets/img/kafa.png"
-        },
-        {
-            productId: 2,
-            productName: "Čaj",
-            productCode: "K-85",
-            category: "Topli napici",
-            price: 1.5,
-            rating: 3.2,
-            imageUrl: "assets/img/tea.png"
-        },
-        {
-            productId: 3,
-            productName: "Coca cola",
-            productCode: "C-11",
-            category: "Bezalkoholna pića",
-            price: 3.5,
-            rating: 4.3,
-            imageUrl: "assets/img/coke.png"
-        },
-        {
-            productId: 4,
-            productName: "Fanta",
-            productCode: "F-41",
-            category: "Bezalkoholna pića",
-            price: 3,
-            rating: 2.5,
-            imageUrl: "assets/img/fanta.png"
-        },
-    ]
-    date: any = new Date();
-    showImage: boolean = false;
+  products: any[] = [
+    {
+      productId: 1,
+      productName: 'Kafa',
+      productCode: 'K-12',
+      category: 'Topli napici',
+      price: 2,
+      rating: 4.8,
+      imageUrl: 'assets/img/kafa.png',
+    },
+    {
+      productId: 2,
+      productName: 'Čaj',
+      productCode: 'K-85',
+      category: 'Topli napici',
+      price: 1.5,
+      rating: 3.2,
+      imageUrl: 'assets/img/tea.png',
+    },
+    {
+      productId: 3,
+      productName: 'Coca cola',
+      productCode: 'C-11',
+      category: 'Bezalkoholna pića',
+      price: 3.5,
+      rating: 4.3,
+      imageUrl: 'assets/img/coke.png',
+    },
+    {
+      productId: 4,
+      productName: 'Fanta',
+      productCode: 'F-41',
+      category: 'Bezalkoholna pića',
+      price: 3,
+      rating: 2.5,
+      imageUrl: 'assets/img/fanta.png',
+    },
+  ];
+  date: any = new Date();
+  showImage: boolean = false;
+  imageWidth: number = 50;
+  imageRadius: number = 50;
 
-    toggleImage() {
-        this.showImage = !this.showImage;
-    }
+  toggleImage() {
+    this.showImage = !this.showImage;
+  }
+  zoomImage() {
+    if (this.imageWidth < 100) {
+      this.imageWidth += 10;
+    } else this.imageWidth -= 10;
+  }
 }
